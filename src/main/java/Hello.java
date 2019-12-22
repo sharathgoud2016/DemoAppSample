cb3b43a587b3bf8f1af57972db7d4780ad9a90cf
Sharath <sharathgoud2016@gmail.com>
2019-12-22 21:33:43 +0530

    Message committed

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World Sharath";
+      message = "Hello World Bharath";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
